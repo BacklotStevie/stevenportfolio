35b86293a47dc7e7803b449929b2d68a3ac4a5c4
Steven Sarria <ssarr012@fiu.edu>
2021-06-16 22:47:04 -0400

    Initial commit

---
+++ schemas/projects.js
@@ -0,0 +1,45 @@
+export default {
+    name: 'hello',
+    title: 'Project',
+    type: 'document',
+    fields: [
+        {
+            name: '',
+            title: '',
+            type: '',
+        },
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 100
+            }
+        },
+        {
+            name: 'image',
+            title: 'Project Screenshot',
+            type: 'image',
+            options: {
+                hotspot: true
+            }
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        },
+        {
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: {type: 'author'}
+        },
+    ]
+}
